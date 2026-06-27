acd50c3a31ae95a2f9b6e6e64ec6993152594067
Anshika <anshikarana1581@gmail.com>
2026-06-28 00:40:49 +0530

    port changes for deployement

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,26 @@ import chatRoutes from "./routes/chat.js";
 import authRoutes from "./routes/auth.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+
 app.use("/api", chatRoutes);
 app.use("/api/auth", authRoutes);
-
-app.listen(PORT, () => {
-  console.log(`server running on ${PORT}`);
-  connectDB();
-});
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connected to database");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   } catch (err) {
-    console.log("Failed to connect with db", err);
+    console.error("Failed to connect with DB:", err);
+    process.exit(1);
   }
 };
+
+connectDB();
